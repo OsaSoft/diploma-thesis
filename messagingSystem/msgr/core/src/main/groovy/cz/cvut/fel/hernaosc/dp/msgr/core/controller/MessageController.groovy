0611f7ee88c814bb1f540b386097893faefe835b
Oscar Hernández <oscar.hernandez@improvisio.cz>
2018-05-17 11:16:01 +0200

    Added controller to send messages and notification through rest endpoints

---
+++ messagingSystem/msgr/core/src/main/groovy/cz/cvut/fel/hernaosc/dp/msgr/core/controller/MessageController.groovy
@@ -0,0 +1,46 @@
+package cz.cvut.fel.hernaosc.dp.msgr.core.controller
+
+import cz.cvut.fel.hernaosc.dp.msgr.core.dto.message.DataMessageDto
+import cz.cvut.fel.hernaosc.dp.msgr.core.dto.message.NotificationDto
+import cz.cvut.fel.hernaosc.dp.msgr.core.mq.ISender
+import cz.cvut.fel.hernaosc.dp.msgr.core.service.IMessagingService
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/send")
+@Slf4j
+class MessageController {
+
+    @Autowired
+    IMessagingService messagingService
+
+    @Autowired
+    ISender<String, String> sender
+
+    @RequestMapping(path = "/notification", method = RequestMethod.POST)
+    void sendNotification(@RequestBody NotificationDto notificationDto) {
+        log.debug "Received notification $notificationDto"
+
+        ["Devices", "Users", "Groups"].each {
+            if (notificationDto."target$it") {
+                messagingService."sendNotification${it}Ids"(notificationDto.title, notificationDto.body, notificationDto."target$it")
+            }
+        }
+    }
+
+    @RequestMapping(path = "/message", method = RequestMethod.POST)
+    void sendMessage(@RequestBody DataMessageDto dataMessageDto) {
+        log.debug "Received message $dataMessageDto"
+
+        ["Devices", "Users", "Groups"].each {
+            if (dataMessageDto."target$it") {
+                messagingService."sendMessage${it}Ids"(dataMessageDto."target$it", dataMessageDto.content)
+            }
+        }
+    }
+}
--- messagingSystem/msgr/core/src/main/groovy/cz/cvut/fel/hernaosc/dp/msgr/core/dto/message/NotificationDto.groovy
+++ messagingSystem/msgr/core/src/main/groovy/cz/cvut/fel/hernaosc/dp/msgr/core/dto/message/NotificationDto.groovy
@@ -1,5 +1,8 @@
 package cz.cvut.fel.hernaosc.dp.msgr.core.dto.message
 
+import groovy.transform.ToString
+
+@ToString(includeNames = true, includePackage = false, includeSuperProperties = true)
 class NotificationDto extends MessageDto {
     String title
     String body
