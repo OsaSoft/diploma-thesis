f14986ce9a3e3f4891c7f6488b98d5d0c8e199c3
Oscar Hernández <oscar.hernandez@improvisio.cz>
2018-05-19 16:50:43 +0200

    Added configurable amount of activemq connections

--- messagingSystem/msgr/activemq/src/main/groovy/cz/cvut/fel/hernaosc/dp/msgr/activemq/config/ConnectionPoolConfig.groovy
+++ messagingSystem/msgr/activemq/src/main/groovy/cz/cvut/fel/hernaosc/dp/msgr/activemq/config/ConnectionPoolConfig.groovy
@@ -2,16 +2,20 @@ package cz.cvut.fel.hernaosc.dp.msgr.activemq.config
 
 import org.apache.activemq.ActiveMQConnectionFactory
 import org.apache.activemq.pool.PooledConnectionFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ConnectionPoolConfig {
 
+    @Value('${msgr.activemq.max.connections:#{1}}')
+    private int maxConnections = 1
+
     @Bean(name = "pooledConnectionFactory")
     PooledConnectionFactory pooledConnectionFactory() {
         def pooledFactory = new PooledConnectionFactory(new ActiveMQConnectionFactory())
-        pooledFactory.maxConnections = 1
+        pooledFactory.maxConnections = maxConnections
         pooledFactory
     }
 }
